e01430dab1f68ba3580424f65d87f52bd84d0ae3
kiyeop yang <kiyeopyang@gmail.com>
2019-12-07 13:02:28 +0900

    tesseract

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const Tesseract = require("tesseract.js");
 
 const PORT = 80;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json("Hello World");
+app.get("/", async (req, res) => {
+  try {
+    const { url } = req.query;
+    const {
+      data: { text },
+    } = await Tesseract.recognize(url, "eng", { logger: m => console.log(m) });
+    res.json(text);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json(e.message);
+  }
 });
 
 app.listen(PORT, () => {
